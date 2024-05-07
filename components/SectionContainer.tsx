7f09be96dd1f68a6ab73e275799324b93da4601d
Waleed Tahir <156667045+waleedtahir399@users.noreply.github.com>
2024-05-07 22:33:33 +0500

    Initial commit

---
+++ components/SectionContainer.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react'
+
+interface Props {
+  children: ReactNode,
+  noContainer : boolean
+}
+
+export default function SectionContainer({ children   , noContainer}: Props) {
+  return (
+    <section className={noContainer ? "":"custom-container"}>{children}</section>
+  )
+}
