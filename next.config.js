83d3290a8479db6affd35ab2ce8adf8b79a9a7dd
Waleed Tahir <tahirwaleed399@gmail.com>
2024-05-17 19:43:27 +0500

    next config update

--- next.config.js
+++ next.config.js
@@ -2,108 +2,34 @@ const { withContentlayer } = require('next-contentlayer2')
 const createNextIntlPlugin = require('next-intl/plugin')
 
 const withNextIntl = createNextIntlPlugin()
-const nextConfig = {}
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
-// You might need to insert additional domains in script-src if you are using external services
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
-  style-src 'self' 'unsafe-inline';
-  img-src * blob: data:;
-  media-src *.s3.amazonaws.com;
-  connect-src *;
-  font-src 'self';
-  frame-src giscus.app
-`
-
-const securityHeaders = [
-  {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\n/g, ''),
+const nextConfig = {
+  assetPrefix: 'https://about-us.gutricious.com/',
+  // assetPrefix: 'http://localhost:3000/',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
   },
-  {
-    key: 'Referrer-Policy',
-    value: 'strict-origin-when-cross-origin',
-  },
-  {
-    key: 'X-Frame-Options',
-    value: 'DENY',
-  },
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
-  },
-  {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on',
-  },
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=31536000; includeSubDomains',
-  },
-  {
-    key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=()',
-  },
-]
-
-/**
- * @type {import('next/dist/next-server/server/config').NextConfig}
- **/
-const getNextIntl = () => withNextIntl(nextConfig)
-module.exports = () => {
-  const plugins = [withContentlayer, withBundleAnalyzer, getNextIntl]
-  return plugins.reduce((acc, next) => next(acc), {
-    reactStrictMode: true,
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-    eslint: {
-      dirs: ['app', 'components', 'layouts', 'scripts'],
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'picsum.photos',
-        },
-      ],
-    },
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: [
-            {
-              key: 'Access-Control-Allow-Credentials',
-              value: 'true',
-            },
-            {
-              key: 'Access-Control-Allow-Origin',
-              value: 'https://gutricious.com', // Allow requests from this specific domain
-            },
-            {
-              key: 'Access-Control-Allow-Methods',
-              value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-            },
-            {
-              key: 'Access-Control-Allow-Headers',
-              value:
-                'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-            },
-          ],
-        },
-      ]
-    },
-    webpack: (config, options) => {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      })
-
-      return config
-    },
-  })
 }
+
+module.exports = withContentlayer(withNextIntl(nextConfig))
+
+// const getNextIntl =  ()=> withContentlayer(withNextIntl(nextConfig));
+// module.exports = () => {
+//   const plugins = [withContentlayer, withBundleAnalyzer ,getNextIntl ]
+//   return plugins.reduce((acc, next) => next(acc), {
+//     reactStrictMode: true,
+//     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+//     eslint: {
+//       dirs: ['app', 'components', 'layouts', 'scripts'],
+//     },
+
+//   })
+// }
